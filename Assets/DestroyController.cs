f0d0653b90f4c9e0e2ec4f87d0cde322718fde5f
masaki344 <masakimiyoshi18.v.v@icloud.com>
2020-02-17 04:24:31 +0900

    StepAsideUnityChanプロジェクト　３Dゲーム作成
    課題の提出

---
+++ Assets/DestroyController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyController : MonoBehaviour
+{
+    //不要になったアイテムを順次破棄しましょう
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    private void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }
+}
