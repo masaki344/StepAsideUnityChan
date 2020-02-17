b9a15f2c9156d766e3543e467b395d26111f61f9
masaki344 <masakimiyoshi18.v.v@icloud.com>
2020-02-17 18:31:01 +0900

    StepAsideUnityChanプロジェクト　３Dゲーム作成
    課題の再提出

--- Assets/DestroyController.cs
+++ Assets/DestroyController.cs
@@ -5,18 +5,17 @@ using UnityEngine;
 public class DestroyController : MonoBehaviour
 {
     //不要になったアイテムを順次破棄しましょう
+    private GameObject cam;
     void Start ()
     {
-		
+        this.cam = GameObject.Find("Main Camera");
 	}
 	
 	void Update ()
     {
-		
+        if (this.transform.position.z < this.cam.transform.position.z)
+        {
+            Destroy(this.gameObject);
+        }
 	}
-
-    private void OnBecameInvisible()
-    {
-        Destroy(this.gameObject);
-    }
 }
--- Assets/ItemGenerator.cs
+++ Assets/ItemGenerator.cs
@@ -47,6 +47,6 @@ public class ItemGenerator : MonoBehaviour
 	
 	void Update ()
     {
-		
-	}
+
+    }
 }
